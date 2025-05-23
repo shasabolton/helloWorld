fd640e37362bd58b4e73db48ab433cdfa6db5afc
shasabolton <shasabolton@gmail.com>
2025-05-23 16:42:05 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var div = document.createElement("div");
-div.innerHTML = "created a div with js4";
+div.innerHTML = "created a div with js7";
 document.body.append(div);
 
 //alert("alert from inside script.js");
-//console.log("log from inside script.js");
+console.log("log from inside script.js");
